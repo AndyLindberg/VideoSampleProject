fb385e4cd6974546772e7fe9b18cfc58aa95ee6e
Andy Lindberg <andy@baronweather.com>
2022-11-04 09:44:28 -0500

    Do not use main.storyboard

--- VideoTestApp/VideoTestApp/AppDelegate.swift
+++ VideoTestApp/VideoTestApp/AppDelegate.swift
@@ -7,21 +7,21 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    //var window: UIWindow?
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//
-//        self.window = UIWindow(frame: UIScreen.main.bounds)
-//        let nav1 = UINavigationController()
-//        let mainView = ProgramaticViewController() //ViewController = Name of your controller
-//        nav1.viewControllers = [mainView]
-//        self.window!.rootViewController = nav1
-//        self.window?.makeKeyAndVisible()
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let nav1 = UINavigationController()
+        let mainView = StackViewController() //ViewController = Name of your controller
+        nav1.viewControllers = [mainView]
+        self.window!.rootViewController = nav1
+        self.window?.makeKeyAndVisible()
         return true
     }
 
